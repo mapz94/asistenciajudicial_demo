8e2343b5edc193d04b94b4929137f762f6a39a81
miguel-apg <51242040+miguel-apg@users.noreply.github.com>
2021-12-29 10:31:54 -0300

    first commit

---
+++ _nuxt/867c677.js
@@ -0,0 +1,2 @@
+/*! For license information please see LICENSES */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{413:function(e,n,r){"use strict";r.r(n);var o=r(378);r.d(n,"default",(function(){return o.a}));o.a.registerVersion("firebase","9.6.1","app-compat")}}]);
